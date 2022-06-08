55dc674c541a0d4a4fa9649f179c8ecb3c04fdbe
WmmyS <wesleymarcosm@gmail.com>
2022-06-08 00:39:41 -0300

    feat: Added nodemon.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("The sedulous hyena ate the antelope!");
+app.get('/', (req, res) => {
+  res.send('The sedulous hyena ate the antelope!');
+});
+
+app.get('/wesley', (req, res) => {
+  res.send('test');
 });
 
 app.listen(port, () => {
-    console.log(`server is listening on ${port}`);
+  console.log(`server is listening on ${port}`);
 });
